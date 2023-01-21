5eb1bddb4c83f1235b38adeeb46c57b3d80c2cae
This pc <This pc@DESKTOP-J2U5699>
2023-01-21 09:59:20 -0800

    implemented jdbc crud operations

--- JDBC/src/com/ineuron/main/Test.java
+++ JDBC/src/com/ineuron/main/Test.java
@@ -1,14 +1,33 @@
 package com.ineuron.main;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.sql.*;
 
 public class Test {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
+
+		ResultSet resultSet=null;
 		
-		FileWriter fr= new FileWriter("gfdc.txt");
-		fr.write("kjhg");
-		fr.close();
+	  Class.forName("com.mysql.cj.jdbc.Driver");	
+      System.out.println("driver class loaded successfully by DriverManager");
+      Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/pwskills","root","Qpalzm231998@");
+      Statement statement=connection.createStatement();
+      boolean bool=statement.execute("update student set studentid=1003 where studentname='gaurav'");
+      
+      if(bool=true) {
+    	  resultSet=statement.executeQuery("Select * from student");
+    	  while(resultSet.next()) {
+    		  String s=resultSet.getString(1);
+    		  int i   = resultSet.getInt(2);
+    		  System.out.println(s+" "+i);
+    	  }
+      }
+      else {
+    	  int rowsUpdateCount=statement.getUpdateCount();
+    	  System.out.println("rows updated:- "+ rowsUpdateCount);
+      }
+      int deletedRows=statement.executeUpdate("insert into student values('gaurav singh kuntal',123)");
+      System.out.println(connection.getClass().getName()+" "+ resultSet.getClass().getName()+" "+statement.getClass().getName());
 	}
 }
